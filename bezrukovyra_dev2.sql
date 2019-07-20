b94b3b0c0f26e40d8af087b1f369bec797b61897
bezrukov <bezrukov-yra@mail.ru>
2019-07-20 12:05:14 +0600

    sql dump

--- bezrukovyra_dev2.sql
+++ bezrukovyra_dev2.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Июл 15 2019 г., 07:10
--- Версия сервера: 5.7.21-20-beget-5.7.21-20-1-log
--- Версия PHP: 5.6.38
+-- Время создания: Июл 17 2019 г., 20:11
+-- Версия сервера: 5.6.39-83.1
+-- Версия PHP: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `mtk25_termin`
+-- База данных: `bezrukovyra_dev2`
 --
 
 -- --------------------------------------------------------
@@ -27,13 +25,11 @@ SET time_zone = "+00:00";
 --
 -- Структура таблицы `dayoffs`
 --
--- Создание: Июл 10 2019 г., 08:44
---
 
-DROP TABLE IF EXISTS `dayoffs`;
-CREATE TABLE `dayoffs` (
+CREATE TABLE IF NOT EXISTS `dayoffs` (
   `year` varchar(255) NOT NULL,
-  `dates` text NOT NULL
+  `dates` text NOT NULL,
+  PRIMARY KEY (`year`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -41,19 +37,16 @@ CREATE TABLE `dayoffs` (
 --
 
 INSERT INTO `dayoffs` (`year`, `dates`) VALUES
-('2019', '[\"2019-10-04\",\"2019-10-11\",\"2019-10-18\",\"2019-10-25\",\"2019-10-31\",\"2019-10-30\",\"2019-10-29\",\"2019-10-28\",\"2019-04-05\",\"2019-04-12\",\"2019-04-19\",\"2019-04-25\",\"2019-04-24\",\"2019-04-23\",\"2019-04-22\"]');
+('2019', '[\"2019-04-04\",\"2019-04-11\",\"2019-04-18\",\"2019-04-24\",\"2019-04-23\",\"2019-04-22\",\"2019-10-04\",\"2019-10-11\",\"2019-10-18\",\"2019-10-25\",\"2019-10-31\",\"2019-10-30\",\"2019-10-29\",\"2019-10-28\",\"2019-02-15\",\"2019-02-21\",\"2019-01-09\",\"2019-01-10\",\"2019-04-12\",\"2019-07-18\"]');
 
 -- --------------------------------------------------------
 
 --
 -- Структура таблицы `termins`
 --
--- Создание: Июл 05 2019 г., 12:13
---
 
-DROP TABLE IF EXISTS `termins`;
-CREATE TABLE `termins` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `termins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` varchar(10) NOT NULL,
   `hour` varchar(2) NOT NULL,
   `minute` varchar(2) NOT NULL,
@@ -67,8 +60,9 @@ CREATE TABLE `termins` (
   `zuweiser` varchar(50) NOT NULL,
   `comments` varchar(300) NOT NULL,
   `userRegister` varchar(30) NOT NULL,
-  `visitDateNumber` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `visitDateNumber` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `termins`
@@ -82,17 +76,15 @@ INSERT INTO `termins` (`id`, `date`, `hour`, `minute`, `fam`, `name`, `birthday`
 --
 -- Структура таблицы `users`
 --
--- Создание: Июл 05 2019 г., 12:13
---
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `role` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
@@ -101,45 +93,6 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `password`, `email`, `role`) VALUES
 (3, 'Иванов Сергей', '7694f4a66316e53c8cdd9d9954bd611d', 'q@q.q', 1),
 (9, 'Петров Николай', 'f1290186a5d0b1ceab27f4e77c0c5d68', 'w@w.w', 0);
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `dayoffs`
---
-ALTER TABLE `dayoffs`
-  ADD PRIMARY KEY (`year`);
-
---
--- Индексы таблицы `termins`
---
-ALTER TABLE `termins`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `termins`
---
-ALTER TABLE `termins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
---
--- AUTO_INCREMENT для таблицы `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
